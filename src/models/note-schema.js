51d66787b0f1600b8fa1cb0c1c7c0ff73a249b56
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-01-22 13:32:28 -0800

    update

--- src/models/note-schema.js
+++ src/models/note-schema.js
@@ -6,7 +6,7 @@ require('mongoose-schema-jsonschema')(mongoose);
 const note = mongoose.Schema ({
   note: {type:String, required:true},
   user: {type:String, required:true}, 
-  tags: {type:Object},
+  tags: {type:Map},
 });
 
 module.exports = mongoose.model('note', note);
